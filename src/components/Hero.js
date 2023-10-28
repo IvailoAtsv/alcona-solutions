4957ad3d0acf5663a5202931b7f2f974dbe1116b
IvailoAtsv <ivailoatanassovv@gmail.com>
2023-10-28 22:34:10 +0300

    hero done
    for now

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,20 @@
-
+import logo from '../images/logo.png'
+import logoFull from '../images/fullLogo.png'
 
 const Hero = () => {
     return (
-        <div className="w-full object-cover text-center flex justify-start align-center bg-slate-400 h-[450px] ">
-            <div className="flex w-[350px] h-[400px] rounded-md items-center bg-white my-8 mx-8 p-2 gap-4 flex-col justify-evenly">
-                <h1 className="text-3xl font-bold">Вдъхновяващи решения за вашия дом и градина</h1>
-                <p>Добре дошли в Alcona Solutions , вашата врата към изисканите парапети и огради от луксозен комозитен материал. Подобрете вашия екстериор с нашата премиум колекция </p>
-                <button className="py-2 px-6 bg-orange-300 rounded-md">Contact us</button>
+        <div className="w-full text-center flex bg-no-repeat bg-right bg-cover justify-center items-center bg-hero h-screen md:h-4/5 ">
+
+            <div className='w-11/12 h-[90%] md:flex-row flex flex-col-reverse md:justify-between justify-evenly items-center gap-4'>
+
+                <div className="flex text-start w-[40%] max-w-[600px] md:max-w-[400px] md:h-[80%] rounded-md items-start bg-cardBg py-4 gap-3 flex-col justify-evenly">
+                    <img className="w-24 ml-4" src={logo} />
+                    <h1 className="text-3xl ml-4 font-bold">Вдъхновяващи решения за вашия дом и градина</h1>
+                    <p className='ml-4'>Добре дошли в Alcona Solutions , вашата врата към изисканите парапети и огради от луксозен комозитен материал. Подобрете вашия екстериор с нашата премиум колекция </p>
+                    <button className="py-2 px-6 ml-4 font-bold bg-orange-400 rounded-md">
+                        Contact us</button>
+                </div>
+                <img src={logoFull} className='w-[50%] sm:pb-0 sm:pr-0 pb-12 pr-8  max-w-[350px] max-h-[250px]' />
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        hero: "url('/src/images/hero.jpg')"
+        hero: "url('/src/images/hero.jpeg')"
       },
       colors: {
+        cardBg: 'rgba(255,255,255,0.7)',
         background: '#35414c',
         background2: '#13294B'
       },

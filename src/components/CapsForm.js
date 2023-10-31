848e15a762b5c165650914f668cd898960aae933
IvailoAtsv <ivailoatanassovv@gmail.com>
2023-11-01 00:43:40 +0200

    big=good

--- src/components/CapsForm.js
+++ src/components/CapsForm.js
@@ -37,27 +37,27 @@ export function CapsForm({
 
             <div className="w-full flex justify-evenly items-center">
                 <div className="flex flex-col justify-center items-center">
-                    <p className=" text-lg font-semibold text-center pb-2">Соларна</p>
-                    <img src={solar} className='w-40 h-40 rounded-md' />
-                    <label className="text-lg">Брой:  </label>
+                    <p className=" text-xl font-semibold text-center pb-2">Соларна</p>
+                    <img src={solar} className='w-60 h-60 rounded-md' />
+                    <label className="text-xl font-semibold">Брой:  </label>
                     <input name="solar" type="number" onChange={(e) => validate(e)} className="w-[160px] h-[30px] bg-gray-200 text-xl text-center rounded-md px-2 py-1" />
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                    <p className=" text-lg font-semibold text-center pb-2">LED 12v</p>
-                    <img name="Caps" src={led} className='w-40 h-40 rounded-md' />
-                    <label className="text-lg">Брой: </label>
+                    <p className=" text-xl font-semibold text-center pb-2">LED 12v</p>
+                    <img name="Caps" src={led} className='w-60 h-60 rounded-md' />
+                    <label className="text-xl font-semibold">Брой: </label>
                     <input type="number" name="led" onChange={(e) => validate(e)} className="w-[160px] h-[30px] bg-gray-200 text-xl text-center rounded-md px-2 py-1" />
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                    <p className=" text-lg font-semibold text-center pb-2">Пластмасa</p>
-                    <img src={plastic} className='w-40 h-40 rounded-md' />
-                    <label className="text-lg">Брой:  </label>
+                    <p className=" text-xl font-semibold text-center pb-2">Пластмасa</p>
+                    <img src={plastic} className='w-60 h-60 rounded-md' />
+                    <label className="text-xl font-semibold">Брой:  </label>
                     <input type="number" name="plastic" onChange={(e) => validate(e)} className="w-[160px] h-[30px] bg-gray-200 text-xl text-center rounded-md px-2 py-1" />
                 </div>
             </div>
             {/* <div className="flex gap-2">
                 <input type="radio" />
-                <label className="text-lg">За всички панели ({panelCount})</label>
+                <label className="text-xl font-semibold">За всички панели ({panelCount})</label>
             </div> */}
             <p className="text-2xl text-center mt-4 text-red-600 font-bold">{!isValid ? 'Общия брой на шапките не трябва да надвишава броя на панелите!' : ''}</p>
         </FormWrapper>

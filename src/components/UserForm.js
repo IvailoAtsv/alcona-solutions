0daf7640d69f4688de3098e3b8e28d8be53cefb1
IvailoAtsv <ivailoatanassovv@gmail.com>
2023-11-11 14:40:31 +0200

    up

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,20 +1,12 @@
 import { useEffect, useState } from "react";
 import { FormWrapper } from "./FormWrapper";
 
-export const UserForm = ({
-  isValid,
-  setIsValid,
-  cartItems,
-  orderStatus,
-  setOrderStatus,
-  userData,
-  setUserData,
-}) => {
+export const UserForm = ({ cartItems, userData, setUserData }) => {
   const inputStyle =
-    "border-2 px-9 py-2 sm:w-[80%] max-w-[400px] w-[90%] rounded-md text-black foucs:border-background focus:ring-0 focus:outline-background";
+    "border-2 px-9 py-[4px] sm:w-[80%] max-w-[400px] w-[90%] rounded-md text-black foucs:border-background focus:ring-0 focus:outline-background";
 
-  const labelErrorStyle = "text-xl text-center mt-4 text-red-600 font-bold";
-  const labelStyle = "text-xl text-center mt-4 font-semibold";
+  const labelErrorStyle = "text-md text-center mt-4 text-red-600 font-bold";
+  const labelStyle = "text-md text-center mt-4 font-semibold";
 
   // const initial = {
   //   name: '',
@@ -36,6 +28,8 @@ export const UserForm = ({
   const [isCityValid, setIsCityValid] = useState(true);
   const [isAreaValid, setIsAreaValid] = useState(true);
   const [clickable, setClickable] = useState(false);
+
+  const [status, setStatus] = useState("info");
 
   function updateFields(fields) {
     setUserData((prev) => {
@@ -132,83 +126,89 @@ export const UserForm = ({
   }, [userData]);
   return (
     <div className="flex flex-col w-full py-0 sm:min-h-[450px] h-11/12 justify-around  items-center">
-      <h1 className="text-3xl mb-10 font-semibold border-b-4 pb-1 px-4 border-background">
-        Данни за Поръчка
-      </h1>
-      <div className="flex flex-col w-full h-full justify-evenly items-center">
-        {/* <p>Доставка до адрес (доплащане )</p> */}
-        <div className="flex flex-col w-[90%] justify-evenly max-w-[500px] rounded-md bg-gray-100 items-center">
-          <label className={isNameValid ? labelStyle : labelErrorStyle}>
-            {!isNameValid ? "Въведете валидно име" : "* Име: "}
-          </label>
-          <input
-            value={name}
-            name="name"
-            onChange={(e) => validateName(e)}
-            className={inputStyle}
-          />
+      {status === "info" ? (
+        <>
+          <h1 className="text-3xl mb-10 font-semibold border-b-4 pb-1 px-4 border-background">
+            Данни за Поръчка
+          </h1>
+          <div className="flex flex-col w-full h-full justify-evenly items-center">
+            {/* <p>Доставка до адрес (доплащане )</p> */}
+            <div className="flex flex-col w-[90%] justify-evenly max-w-[500px] rounded-md bg-gray-100 items-center">
+              <label className={isNameValid ? labelStyle : labelErrorStyle}>
+                {!isNameValid ? "Въведете валидно име" : "* Име: "}
+              </label>
+              <input
+                value={name}
+                name="name"
+                onChange={(e) => validateName(e)}
+                className={inputStyle}
+              />
 
-          <label className={isPhoneValid ? labelStyle : labelErrorStyle}>
-            {!isPhoneValid ? "Въведете валиден телефон" : "* Телефон: "}
-          </label>
-          <input
-            name="phone"
-            value={phone}
-            onChange={(e) => validatePhone(e)}
-            className={inputStyle}
-          />
+              <label className={isPhoneValid ? labelStyle : labelErrorStyle}>
+                {!isPhoneValid ? "Въведете валиден телефон" : "* Телефон: "}
+              </label>
+              <input
+                name="phone"
+                value={phone}
+                onChange={(e) => validatePhone(e)}
+                className={inputStyle}
+              />
 
-          <button
-            name="pickUp"
-            type="submit"
-            onClick={handlePickup}
-            className="rounded-lg py-2 my-4 px-6 border-4 self-center font-bold duration-500 border-black hover:bg-black hover:text-white w-[90%] max-w-[400px] sm:w-[80%]"
-          >
-            Взимане от склад
-          </button>
-        </div>
-        <p className="py-2">Или</p>
-        <div className="flex rounded-md max-w-[500px] bg-gray-100 flex-col w-[90%] justify-evenly items-center">
-          <label className={isEmailValid ? labelStyle : labelErrorStyle}>
-            {!isEmailValid ? "Въведете валиден e-mail" : "* e-mail: "}
-          </label>
-          <input
-            value={email}
-            name="email"
-            onChange={(e) => validateEmail(e)}
-            className={inputStyle}
-          />
+              <button
+                name="pickUp"
+                type="submit"
+                onClick={handlePickup}
+                className="rounded-lg py-[4px] my-4 px-6 border-4 self-center font-bold duration-500 border-black hover:bg-black hover:text-white w-[90%] max-w-[400px] sm:w-[80%]"
+              >
+                Взимане от склад
+              </button>
+            </div>
+            <p className="py-2">Или</p>
+            <div className="flex rounded-md max-w-[500px] bg-gray-100 flex-col w-[90%] justify-evenly items-center">
+              <label className={isEmailValid ? labelStyle : labelErrorStyle}>
+                {!isEmailValid ? "Въведете валиден e-mail" : "* e-mail: "}
+              </label>
+              <input
+                value={email}
+                name="email"
+                onChange={(e) => validateEmail(e)}
+                className={inputStyle}
+              />
 
-          <label className={isCityValid ? labelStyle : labelErrorStyle}>
-            {!isCityValid ? "Въведете валиден град" : "* Град: "}
-          </label>
-          <input
-            name="city"
-            value={city}
-            onChange={(e) => validateCity(e)}
-            className={inputStyle}
-          />
+              <label className={isCityValid ? labelStyle : labelErrorStyle}>
+                {!isCityValid ? "Въведете валиден град" : "* Град: "}
+              </label>
+              <input
+                name="city"
+                value={city}
+                onChange={(e) => validateCity(e)}
+                className={inputStyle}
+              />
 
-          <label className={isAreaValid ? labelStyle : labelErrorStyle}>
-            {!isAreaValid ? "Въведете валиден окръг" : "* Окръг: "}
-          </label>
-          <input
-            value={area}
-            onChange={(e) => validateArea(e)}
-            name="area"
-            className={inputStyle}
-          />
-          <button
-            name="pickUp"
-            disabled={!clickable}
-            type="submit"
-            onClick={handleDelivery}
-            className="rounded-lg py-2 px-6 my-5 border-4 self-center font-bold duration-500 border-black hover:bg-black hover:text-white w-[90%] max-w-[400px] sm:w-[80%]"
-          >
-            Доставка на адрес
-          </button>
-        </div>
-      </div>
+              <label className={isAreaValid ? labelStyle : labelErrorStyle}>
+                {!isAreaValid ? "Въведете валиден окръг" : "* Окръг: "}
+              </label>
+              <input
+                value={area}
+                onChange={(e) => validateArea(e)}
+                name="area"
+                className={inputStyle}
+              />
+              <button
+                name="pickUp"
+                disabled={!clickable}
+                type="submit"
+                onClick={handleDelivery}
+                className="rounded-lg py-[4px] px-6 my-5 border-4 self-center font-bold duration-500 border-black hover:bg-black hover:text-white w-[90%] max-w-[400px] sm:w-[80%]"
+              >
+                Доставка на адрес
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
